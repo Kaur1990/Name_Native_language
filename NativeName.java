dc8789350e146083a0c1b81969a9f3071324171e
Kaur1990 <kapoor.raman1990@gmail.com>
2024-01-11 23:26:19 +0530

    Name in native language

---
+++ NativeName.java
@@ -0,0 +1,13 @@
+public class NativeName
+{
+public static void main(String args[])
+{
+char ch='\u0930';
+char ch1 ='\u092E';
+char ch2 ='\u0928';
+char ch3 ='\u0926';
+char ch4 ='\u0940';
+char ch5 ='\u092A';
+System.out.println(ch+""+ch1+""+ch2+""+ch3+""+ch4+""+ch5);
+}
+}
